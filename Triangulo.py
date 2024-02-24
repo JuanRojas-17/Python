fe807f5f781d1683b947438786291ebc43a52d60
JuanRojas-17 <juankisrojass@gmail.com>
2024-02-23 21:43:02 -0500

    Ejercicio 7

---
+++ Triangulo.py
@@ -0,0 +1,8 @@
+print("Introduce los siguientes datos para calcular el area de tu triangulo")
+
+base = int(input("Base del triangulo:"))
+altura = int(input("Altura del triangulo"))
+
+operacion = (base*altura)/2
+
+print("El area de tu triangulo es:", operacion)
