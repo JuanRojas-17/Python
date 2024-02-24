0dc6a62b0e15f9ae55496abb0f53e7e4b2ab4658
JuanRojas-17 <juankisrojass@gmail.com>
2024-02-23 20:40:19 -0500

    Ejercicio 2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-Numero1 = int (input ("Ingrese su primer numero:"))
-Numero2 = int (input ("Ingrese su segundo numero:"))
-
-suma = Numero1 + Numero2
-
-print (suma)
+numero1 = int(input("Escriba su primer numero:"))
+numero2 = int(input("Escriba su segundo numero:"))
+numero3 = int(input("Escriba su tercer numero:"))
+numeroMayor = max(numero1, numero2, numero3)
+print("El numero mayor entre los tres es:", numeroMayor)
