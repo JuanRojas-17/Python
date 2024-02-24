0b414033c43912cf6727595bd08d90ee7b46dc4e
JuanRojas-17 <juankisrojass@gmail.com>
2024-02-23 20:46:37 -0500

    Ejercicio 2

---
+++ NumeroMayor.py
@@ -0,0 +1,7 @@
+numero1=int(input("Escriba su primer numero:"))
+numero2=int(input("Escriba su segundo numero:"))
+numero3=int(input("Escriba su tercer numero:"))
+
+numeroMayor=max(numero1, numero2, numero3)
+
+print("El numero mayor entre los tres numeros es:", numeroMayor)
